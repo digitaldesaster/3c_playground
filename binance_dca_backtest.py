ad94b443d4a535db1f03596b769416b5d94d0ad0
Alex <digitald74@gmail.com>
2021-06-20 14:55:37 +0200

    Update binance_dca_backtest.py

--- binance_dca_backtest.py
+++ binance_dca_backtest.py
@@ -14,6 +14,8 @@ dca_tp = 0.5
 dca_sos = 0.5
 dca_ss = 1.15
 
+dca_fee = 0.2
+
 dca_so_count=0
 dca_last_so = 0
 dca_current_so=0
@@ -34,7 +36,7 @@ def on_message(ws, message):
     handle_trades(current_price)
 
 def handle_trades(current_price):
-    global capital,bot_total_volume,bot_total_coins,dca_bo,dca_so,dca_mstc,dca_os,dca_tp,dca_sos,dca_ss,last_bought_price,dca_current_so,dca_so_count,dca_last_so,next_so_buy_price,bot_avg_price,sell_price
+    global capital,bot_total_volume,bot_total_coins,dca_bo,dca_so,dca_mstc,dca_os,dca_tp,dca_sos,dca_ss,last_bought_price,dca_current_so,dca_so_count,dca_last_so,next_so_buy_price,bot_avg_price,sell_price,dca_fee
     if bot_total_volume==0:
         buy_amount = dca_bo / float(current_price)
         bot_avg_price = current_price
@@ -43,6 +45,7 @@ def handle_trades(current_price):
         sell_price = current_price + (current_price / 100 * dca_tp)
         bot_total_volume = bot_total_volume + (buy_amount * current_price)
         capital = capital - dca_bo
+        capital = capital - (dca_bo/100*dca_fee)
         print (f"we are buying {buy_amount} of {pair}")
         print (f"we now have {bot_total_coins} of {pair}")
         print (f"we will buy more at {next_so_buy_price}")
@@ -58,6 +61,7 @@ def handle_trades(current_price):
                     dca_current_so +=1
                     buy_amount = dca_so / float(current_price)
                     capital = capital - dca_so
+                    capital = capital - (dca_so/100*dca_fee)
                     dca_last_so = dca_so
                     bot_total_coins += buy_amount
                     next_so_buy_price = current_price - (current_price / 100 * dca_sos)
@@ -82,6 +86,7 @@ def handle_trades(current_price):
                     dca_current_so +=1
                     dca_last_so = dca_last_so * dca_os
                     capital = capital - dca_last_so
+                    capital = capital - (dca_last_so/100*dca_fee)
                     buy_amount = dca_last_so / float(current_price)
                     bot_total_coins += buy_amount
                     next_so_buy_price = current_price - (current_price / 100 * dca_sos)
@@ -192,10 +197,10 @@ def downloadPriceData(symbol):
 #downloadPriceData('MATICUSDT')
 
 #This function processes the local data
-#processLocalData('MATICUSDT')
+processLocalData('MATICUSDT')
 
 #This function get the last 1500 datapoints (limit)...
-get_historical_data('MATICUSDT','1m',1500)
+#get_historical_data('MATICUSDT','1m',1500)
 
 
 #LIVE SIMULATION!!!!
